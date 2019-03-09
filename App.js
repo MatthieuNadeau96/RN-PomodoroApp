484aec80f653d2d9976b724211ce521b17f31e9c
Matthieu <matthieunadeau96@gmail.com>
2019-03-09 12:06:17 -0500

    Stripped HomeScreen from it's logic and put it back into App.js to simplify the issue with passing props

--- App.js
+++ App.js
@@ -5,16 +5,159 @@ import { createStackNavigator, createAppContainer, createBottomTabNavigator } fr
 import HomeScreen from './src/screens/HomeScreen'
 import SettingsScreen from './src/screens/SettingsScreen'
 
+
+
 class Home extends React.Component {
+
+  state = {
+    time: {},
+    counting: false,
+    mode: 'WORK',
+    completedWorkCount: 0,
+    workTotalForBigBreak: 3,
+    seconds: 5,
+  }
+
+  componentDidMount = () => {
+    this.resetDisplay(this.state.seconds)
+  }
+
+  resetDisplay = (secs) => {
+    let timeLeftVar = this.secondsToTime(secs)
+    this.setState({ time: timeLeftVar })
+  }
+
+  secondsToTime = (secs) => {
+    let hours = Math.floor(secs / (60 * 60))
+    let divisor_for_minutes = secs % (60 * 60)
+    let minutes = Math.floor(divisor_for_minutes / 60)
+    let divisor_for_seconds = divisor_for_minutes % 60
+    let seconds = Math.ceil(divisor_for_seconds)
+
+    if (seconds < 10) {
+      seconds = '0' + seconds
+    }
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    }
+    return obj
+  }
+
+  countDown = () => {
+    const { mode, completedWorkCount, workTotalForBigBreak } = this.state
+    const { workTimer, breakTimer, bigBreakTimer } = this.props
+    console.log("counting down...")
+    let seconds = this.state.seconds - 1
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds, // <-- here is the problem
+    })
+    console.log("time: " + seconds)
+    if (seconds === 0) {
+      // alarm
+      if (mode === 'WORK' && completedWorkCount === workTotalForBigBreak - 1) {
+        this.resetDisplay(bigBreakTimer)
+        this.setState({ mode: 'BIGBREAK', seconds: bigBreakTimer, completedWorkCount: completedWorkCount + 1})
+      }
+      else if (mode === 'WORK' && completedWorkCount !== workTotalForBigBreak) {
+        this.setState({ mode: 'BREAK', seconds: breakTimer, completedWorkCount: completedWorkCount + 1 })
+        this.resetDisplay(breakTimer)
+      }
+      if (mode === 'BREAK') {
+        this.setState({ mode: 'WORK', seconds: workTimer, })
+        this.resetDisplay(workTimer)
+      }
+      if (mode === 'BIGBREAK') {
+        this.setState({ mode: 'WORK', seconds: workTimer, completedWorkCount: 0 })
+        this.resetDisplay(workTimer)
+      }
+      this.pauseTimer()
+    }
+
+  }
+
+  startTimer = () => {
+    const { mode } = this.state
+    this.setState({counting: true})
+    this.myInterval = setInterval(this.countDown, 1000)
+    this.timerCountDown()
+  }
+
+  pauseTimer = () => {
+    clearInterval(this.myInterval)
+    clearInterval(this.progress)
+    this.setState({ counting: false })
+  }
+
+  stopTimer = () => {
+    const { counting, mode } = this.state
+    const { workTimer, breakTimer, bigBreakTimer } = this.props
+
+    this.pauseTimer()
+    if(mode === 'WORK') {
+    this.resetDisplay(workTimer)
+    this.setState({seconds: workTimer})
+    }
+    if (mode === 'BREAK') {
+    this.resetDisplay(breakTimer)
+    this.setState({seconds: breakTimer})
+    }
+    else if (mode === 'BIGBREAK') {
+    this.resetDisplay(bigBreakTimer)
+    this.setState({seconds: bigBreakTimer})
+    }
+    this.setState({progressBarCountdown: 100})
+  }
+
+  timerCountDown = () => {
+    const { seconds } = this.state
+    this.progress = setInterval(() => {
+      if (seconds <= 0) {
+        clearInterval(this.progress)
+      }
+      this.setState( prevState => ({
+        progressBarCountdown: prevState.progressBarCountdown - 1
+      }))
+    }, 1000 / (100 / seconds))
+  }
+
+  skipTimer = () => {
+    const { mode } = this.state
+    const { workTimer, breakTimer, bigBreakTimer } = this.props
+
+    if (mode === 'WORK') {
+      this.setState({ mode: 'BREAK', seconds: breakTimer })
+      this.resetDisplay(breakTimer)
+    }
+    if (mode === 'BREAK') {
+      this.setState({ mode: 'BIGBREAK', seconds: bigBreakTimer })
+      this.resetDisplay(bigBreakTimer)
+    }
+    if (mode === 'BIGBREAK') {
+      this.setState({ mode: 'WORK', seconds: workTimer })
+      this.resetDisplay(workTimer)
+    }
+
+  }
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <HomeScreen
-          seconds={this.props.screenProps.seconds}
           workTimer={this.props.screenProps.workTimer}
           breakTimer={this.props.screenProps.breakTimer}
           bigBreakTimer={this.props.screenProps.bigBreakTimer}
+          completedWorkCount={this.state.completedWorkCount}
+          workTotalForBigBreak={this.state.workTotalForBigBreak}
+          time={this.state.time}
+          counting={this.state.counting}
+          stopTimer={this.stopTimer}
+          pauseTimer={this.pauseTimer}
+          startTimer={this.startTimer}
+          skipTimer={this.skipTimer}
         />
       </View>
     );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -16,136 +16,6 @@ import HeaderButtons, { HeaderButton, Item } from 'react-navigation-header-butto
 
 export default class HomeScreen extends Component {
 
-  state = {
-    time: {},
-    counting: false,
-    mode: 'WORK',
-    completedWorkCount: 0,
-    workTotalForBigBreak: 3
-  }
-
-  componentDidMount = () => {
-    this.resetDisplay(this.props.seconds)
-  }
-
-  resetDisplay = (secs) => {
-    let timeLeftVar = this.secondsToTime(secs)
-    this.setState({ time: timeLeftVar })
-  }
-
-  secondsToTime = (secs) => {
-    let hours = Math.floor(secs / (60 * 60))
-    let divisor_for_minutes = secs % (60 * 60)
-    let minutes = Math.floor(divisor_for_minutes / 60)
-    let divisor_for_seconds = divisor_for_minutes % 60
-    let seconds = Math.ceil(divisor_for_seconds)
-
-    if (seconds < 10) {
-      seconds = '0' + seconds
-    }
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    }
-    return obj
-  }
-
-  countDown = () => {
-    const { mode, completedWorkCount, workTotalForBigBreak } = this.state
-    const { workTimer, breakTimer, bigBreakTimer } = this.props
-    let seconds = this.props.seconds - 1
-    this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds,
-    })
-    if (seconds === 0) {
-      // alarm
-      if (mode === 'WORK' && completedWorkCount === workTotalForBigBreak - 1) {
-        this.resetDisplay(bigBreakTimer)
-        this.setState({ mode: 'BIGBREAK', seconds: bigBreakTimer, completedWorkCount: completedWorkCount + 1})
-      }
-      else if (mode === 'WORK' && completedWorkCount !== workTotalForBigBreak) {
-        this.setState({ mode: 'BREAK', seconds: breakTimer, completedWorkCount: completedWorkCount + 1 })
-        this.resetDisplay(breakTimer)
-      }
-      if (mode === 'BREAK') {
-        this.setState({ mode: 'WORK', seconds: workTimer, })
-        this.resetDisplay(workTimer)
-      }
-      if (mode === 'BIGBREAK') {
-        this.setState({ mode: 'WORK', seconds: workTimer, completedWorkCount: 0 })
-        this.resetDisplay(workTimer)
-      }
-      this.pauseTimer()
-    }
-  }
-
-  startTimer = () => {
-    const { mode } = this.state
-    this.setState({counting: true})
-    this.myInterval = setInterval(this.countDown, 1000)
-    this.timerCountDown()
-  }
-
-  pauseTimer = () => {
-    clearInterval(this.myInterval)
-    clearInterval(this.progress)
-    this.setState({ counting: false })
-  }
-
-  stopTimer = () => {
-    const { counting, mode } = this.state
-    const { workTimer, breakTimer, bigBreakTimer } = this.props
-
-    this.pauseTimer()
-    if(mode === 'WORK') {
-    this.resetDisplay(workTimer)
-    this.setState({seconds: workTimer})
-    }
-    if (mode === 'BREAK') {
-    this.resetDisplay(breakTimer)
-    this.setState({seconds: breakTimer})
-    }
-    else if (mode === 'BIGBREAK') {
-    this.resetDisplay(bigBreakTimer)
-    this.setState({seconds: bigBreakTimer})
-    }
-    this.setState({progressBarCountdown: 100})
-  }
-
-  timerCountDown = () => {
-    const { seconds } = this.props
-    this.progress = setInterval(() => {
-      if (seconds <= 0) {
-        clearInterval(this.progress)
-      }
-      this.setState( prevState => ({
-        progressBarCountdown: prevState.progressBarCountdown - 1
-      }))
-    }, 1000 / (100 / seconds))
-  }
-
-  skipTimer = () => {
-    const { mode } = this.state
-    const { workTimer, breakTimer, bigBreakTimer } = this.props
-
-    if (mode === 'WORK') {
-      this.setState({ mode: 'BREAK', seconds: breakTimer })
-      this.resetDisplay(breakTimer)
-    }
-    if (mode === 'BREAK') {
-      this.setState({ mode: 'BIGBREAK', seconds: bigBreakTimer })
-      this.resetDisplay(bigBreakTimer)
-    }
-    if (mode === 'BIGBREAK') {
-      this.setState({ mode: 'WORK', seconds: workTimer })
-      this.resetDisplay(workTimer)
-    }
-
-  }
-
   render() {
     const iconColor = '#fafafa' // <-- icon font color
     const PlayIcon = (<Icon name="play" size={40} color={iconColor} />)
@@ -157,25 +27,25 @@ export default class HomeScreen extends Component {
         <View style={styles.timerRow}>
           <WorkCounter
             style={styles.workCounter}
-            workCounter={this.state.completedWorkCount}
-            totalWorkCounter={this.state.workTotalForBigBreak}
+            workCounter={this.props.completedWorkCount}
+            totalWorkCounter={this.props.workTotalForBigBreak}
             />
-          <Timer time={this.state.time}/>
+          <Timer time={this.props.time}/>
         </View>
           <View style={styles.buttonRow}>
-            <TouchableOpacity activeOpacity={.8} onPress={this.stopTimer}>
+            <TouchableOpacity activeOpacity={.8} onPress={this.props.stopTimer}>
               <StopSkipButtons title={StopIcon} />
             </TouchableOpacity>
             {
-              this.state.counting ?
-              <TouchableOpacity activeOpacity={.8} onPress={this.pauseTimer}>
+              this.props.counting ?
+              <TouchableOpacity activeOpacity={.8} onPress={this.props.pauseTimer}>
                 <PlayPauseButton title={PauseIcon} />
               </TouchableOpacity> :
-              <TouchableOpacity activeOpacity={.8} onPress={this.startTimer}>
+              <TouchableOpacity activeOpacity={.8} onPress={this.props.startTimer}>
                 <PlayPauseButton title={PlayIcon} />
               </TouchableOpacity>
             }
-            <TouchableOpacity activeOpacity={.8} onPress={this.skipTimer}>
+            <TouchableOpacity activeOpacity={.8} onPress={this.props.skipTimer}>
               <StopSkipButtons title={SkipIcon} />
             </TouchableOpacity>
           </View>
